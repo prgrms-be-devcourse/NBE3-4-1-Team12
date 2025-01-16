e5868f5e2bef91a63a5d571ffa4a036a02598157
Taeheeeeeee <taeheemin62@gmail.com>
2025-01-16 16:32:12 +0900

    feat: coffeebean 재고 로직

--- backend/coffeeBean_backend/src/main/java/com/ll/coffeeBean/domain/coffeeBean/entity/CoffeeBean.java
+++ backend/coffeeBean_backend/src/main/java/com/ll/coffeeBean/domain/coffeeBean/entity/CoffeeBean.java
@@ -1,5 +1,6 @@
 package com.ll.coffeeBean.domain.coffeeBean.entity;
 
+import com.ll.coffeeBean.global.exceptions.ServiceException;
 import com.ll.coffeeBean.global.jpa.entity.BaseTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -25,4 +26,12 @@ public class CoffeeBean extends BaseTime {
 
     @Column
     private Integer quantity;
+
+    // 재고
+    public void reduceStockWithValidation(int orderQuantity) {
+        if (this.quantity < orderQuantity) {
+            throw new ServiceException("400-1", "재고가 부족합니다. 남은 재고: " + this.quantity);
+        }
+        this.quantity -= orderQuantity;
+    }
 }
