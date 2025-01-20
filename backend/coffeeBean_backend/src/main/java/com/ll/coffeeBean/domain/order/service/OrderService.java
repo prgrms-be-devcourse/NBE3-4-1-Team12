aa4e05f9edaa77cc609af68d80afe5df26cca8e0
nodonghui <163218508+nodonghui@users.noreply.github.com>
2025-01-20 11:44:42 +0900

    merge process

--- backend/coffeeBean_backend/src/main/java/com/ll/coffeeBean/domain/order/service/OrderService.java
+++ backend/coffeeBean_backend/src/main/java/com/ll/coffeeBean/domain/order/service/OrderService.java
@@ -35,44 +35,24 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final DetailOrderRepository detailOrderRepository;
     private final PastOrderRepository pastOrderRepository;
-<<<<<<< HEAD
 
 	private final CoffeeBeanService coffeeBeanService;
     private final SiteUserRepository siteUserRepository;
     private final CoffeeBeanRepository coffeeBeanRepository;
-
-
-=======
-    private final CoffeeBeanService coffeeBeanService;
-    private final SiteUserRepository siteUserRepository;
-    private final CoffeeBeanRepository coffeeBeanRepository;
     private final SiteUserRepository userRepository;
->>>>>>> 942f7bb4dda87e5b85e542f3d40ab73c89a88b1e
 
     public long count() {
         return orderRepository.count();
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 942f7bb4dda87e5b85e542f3d40ab73c89a88b1e
     //pageable 설정 후 jpa를 유저 정보와 pageable를 이용해 page 데이터 탐색
     public PageDto<GetResMenuOrderDto> getList(SiteUser siteUser, int page, int pageSize) {
 
         Pageable pageable = PageRequest.of(page, pageSize);
-<<<<<<< HEAD
-
-=======
->>>>>>> 942f7bb4dda87e5b85e542f3d40ab73c89a88b1e
         Page<MenuOrder> paging = this.orderRepository.findByCustomer(pageable, siteUser);
 
         Page<GetResMenuOrderDto> pagingOrderDto = paging.map(GetResMenuOrderDto::new);
         PageDto<GetResMenuOrderDto> pageDto = new PageDto<>(pagingOrderDto);
-<<<<<<< HEAD
-
-=======
->>>>>>> 942f7bb4dda87e5b85e542f3d40ab73c89a88b1e
         return pageDto;
 
     }
@@ -87,10 +67,6 @@ public class OrderService {
         return menuOrder;
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 942f7bb4dda87e5b85e542f3d40ab73c89a88b1e
     /**
      * TODO : 효율적인 스케쥴링 정하기, print -> 로그로 변경하기
      */
@@ -166,16 +142,10 @@ public class OrderService {
         return orderRepository.findById(id);
     }
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 942f7bb4dda87e5b85e542f3d40ab73c89a88b1e
     // 주문 수정 비즈니스 로직 (MenuOrder 안에 있는 DetailOrder 수정)
     @Transactional
     public PutMenuOrderRqDTO modify(MenuOrder menuOrder, PutMenuOrderRqDTO reqDetailOrders) {
         // reqBody 에 담겨있는 주문 목록 받기
-<<<<<<< HEAD
         List<BeanNameQuantityDTO> reqBeansDTOList = reqDetailOrders.getCoffeeOrders();
 
         // 받아온 주문 목록의 각 커피콩 주문 별 처리
@@ -187,58 +157,28 @@ public class OrderService {
             DetailOrder detailOrderToChange = menuOrder.getOrders()
                     .stream()
                     .filter(detailOrder -> detailOrder.getName().equals(coffeeBean.getName()))
-=======
-        List<BeanNameQuantityDTO> beansDTOList = reqDetailOrders.getCoffeeOrders();
-
-        // 받아온 주문들의 각 커피콩 별 주문 처리
-        for (BeanNameQuantityDTO beanNameQuantityDTO : beansDTOList) {
-            // 요청된 수량 변경 해야 하는 id 의 커피콩 찾기
-            DetailOrder beanOrderToChange = menuOrder.getOrders()
-                    .stream()
-                    .filter(beanOrder -> beanOrder.getName().equals(beanNameQuantityDTO.getName()))
->>>>>>> feature_frontend_nodonghui
                     .findFirst().get();
 
             // 재고관련 확인 및 처리
-<<<<<<< HEAD
             int changeQuantity = reqBean.getQuantity() - detailOrderToChange.getQuantity(); // 신규 - 기존
-=======
-            CoffeeBean coffeeBean = coffeeBeanService.findByName(beanNameQuantityDTO.getName());
-            int changeQuantity = beanNameQuantityDTO.getQuantity() - beanOrderToChange.getQuantity(); // 신규 - 기존
->>>>>>> feature_frontend_nodonghui
             coffeeBeanService.changeStockWithValidation(coffeeBean, changeQuantity);
 
             // 커피콩 주문 수량 변경
-<<<<<<< HEAD
             if (reqBean.getQuantity() == 0) {
-=======
-            if (beanNameQuantityDTO.getQuantity() == 0) {
->>>>>>> feature_frontend_nodonghui
                 // 변경 수량이 0이면 아예 DetailOrder 를 삭제 (수량이 0인 주문은 없도록)
                 menuOrder.removeDetail(detailOrderToChange);
                 detailOrderRepository.delete(detailOrderToChange);
             } else {
                 // 실제 수량 변경 로직
-<<<<<<< HEAD
                 detailOrderToChange.setQuantity(reqBean.getQuantity());
-=======
-                beanOrderToChange.setQuantity(beanNameQuantityDTO.getQuantity());
->>>>>>> feature_frontend_nodonghui
             }
         }
 
         // 고객의 현재 주문 상태 DTO 에 담아 반환
-<<<<<<< HEAD
         List<BeanNameQuantityDTO> beanNameQuantityDTOList = new ArrayList<>();
         for (DetailOrder beanOrders : menuOrder.getOrders()) {
             BeanNameQuantityDTO beanNameQuantityDto = new BeanNameQuantityDTO(beanOrders.getName(), beanOrders.getQuantity());
             beanNameQuantityDTOList.add(beanNameQuantityDto);
-=======
-        List<BeanNameQuantityDTO> beanIdQuantityDTOList = new ArrayList<>();
-        for (DetailOrder beanOrders : menuOrder.getOrders()) {
-            BeanNameQuantityDTO beanIdQuantityDto = new BeanNameQuantityDTO(beanOrders.getName(), beanOrders.getQuantity());
-            beanIdQuantityDTOList.add(beanIdQuantityDto);
->>>>>>> feature_frontend_nodonghui
         }
         PutMenuOrderRqDTO orderReqDTO = new PutMenuOrderRqDTO(); // 응답 형식에 따름
         orderReqDTO.setCoffeeOrders(beanNameQuantityDTOList);
