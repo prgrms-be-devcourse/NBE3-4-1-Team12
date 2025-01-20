36c2f18ebacbfdf89e25a735608b2a48d887289f
baekminjin <bhs3392@naver.com>
2025-01-20 09:47:49 +0900

    메뉴 추가와 수정

--- backend/coffeeBean_backend/src/main/java/com/ll/coffeeBean/domain/coffeeBean/service/CoffeeBeanService.java
+++ backend/coffeeBean_backend/src/main/java/com/ll/coffeeBean/domain/coffeeBean/service/CoffeeBeanService.java
@@ -1,5 +1,7 @@
 package com.ll.coffeeBean.domain.coffeeBean.service;
 
+import com.ll.coffeeBean.domain.coffeeBean.dto.CoffeeBeanRequestDTO;
+import com.ll.coffeeBean.domain.coffeeBean.dto.CoffeeBeanResponseDTO;
 import com.ll.coffeeBean.domain.coffeeBean.entity.CoffeeBean;
 import com.ll.coffeeBean.domain.coffeeBean.repository.CoffeeBeanRepository;
 import com.ll.coffeeBean.global.exceptions.ServiceException;
@@ -46,5 +48,37 @@ public class CoffeeBeanService {
         } else {
             throw new ServiceException("404", "CoffeeBean을 찾을 수 없습니다.");
         }
+    }
+
+    public CoffeeBeanResponseDTO createCoffeeBean(CoffeeBeanRequestDTO reqBody) {
+
+
+        if (coffeeBeanRepository.existsByName(reqBody.getName())) {
+            throw new ServiceException("400-1", "이미 존재하는 원두입니다.");
+        }
+
+
+        CoffeeBean coffeeBean = new CoffeeBean(reqBody.getName(), reqBody.getPrice(), reqBody.getQuantity());
+        coffeeBeanRepository.save(coffeeBean);
+
+        return new CoffeeBeanResponseDTO(coffeeBean);
+
+    }
+
+
+    public CoffeeBeanResponseDTO modifyCoffeeBean(CoffeeBean coffeeBean, Integer price, Integer quantity) {
+
+        coffeeBean.setPrice(price);
+
+        //수정할 수량이 기존 원두나 추가된 원두 수량보다 작게 수정하면 오류 발생
+        //조건
+        if(coffeeBean.getQuantity() > quantity){
+            throw new ServiceException("400-1", "수정할 수량이 기존 수량보다 작습니다.");
+        }
+        coffeeBean.setQuantity(quantity);
+
+        coffeeBeanRepository.save(coffeeBean);
+
+        return new CoffeeBeanResponseDTO(coffeeBean);
     }
 }
